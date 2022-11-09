721313cb13ea36689d73c5af82ba4849de0339de
BalaAk11 <bala11bmk@gmail.com>
2022-11-09 15:53:37 +0530

    taskcompleted

--- day1.js
+++ day1.js
@@ -1,7 +1,12 @@
-let num = 123456;
-let reverse = 0;
-while(num>0){
-    reverse = (reverse * 10) + (num % 10);
-    num = parseInt(num / 10);
-}
-console.log(reverse);
+// let num = 123456;
+// let reverse = 0;
+// while(num>0){
+//     reverse = (reverse * 10) + (num % 10);
+//     num = parseInt(num / 10);
+// }
+// console.log(reverse);
+
+let n = 12345;
+let rev = parseInt(n.toString().split('').reverse().join(''))
+console.log(rev);
+
