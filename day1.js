31d5d07fd1273abba87cc0dea9a9e984c5a9a5b6
BalaAk11 <bala11bmk@gmail.com>
2022-11-10 15:50:06 +0530

    file added

--- day1.js
+++ day1.js
@@ -10,11 +10,32 @@
 // let rev = parseInt(n.toString().split('').reverse().join(''))
 // console.log(rev);
 
-let date1 = new Date('11-25-2022');
-let date2 = new Date('11-30-2022');
-function days(date1,date2){
-    let timeDiff = date2.getTime() - date1.getTime();
-    let totalDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24))
-    return totalDays
-}
-console.log(days(date1,date2));
+// let date1 = new Date('11-25-2022');
+// let date2 = new Date('11-30-2022');
+// function days(date1,date2){
+//     let timeDiff = date2.getTime() - date1.getTime();
+//     let totalDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24))
+//     return totalDays
+// }
+// console.log(days(date1,date2));
+
+// task 2
+function myFunction() {
+    var input, filter, ul,a, i, li, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('myUl');
+    li = ul.getElementsByTagName('li');
+    for ( i=0; i<li.length; i++){
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
+    
+    }
+
+
